206f4b341ad2c90802137bddacd560edbeed11bc
Tyler Long <tyler4long@gmail.com>
2021-12-09 16:13:27 -0800

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world');
+let handle: NodeJS.Timeout;
+const detect = () => {
+  console.log('detect', new Date());
+  if (handle) {
+    clearTimeout(handle);
+  }
+  handle = setTimeout(() => detect(), 10000);
+};
+
+detect();
